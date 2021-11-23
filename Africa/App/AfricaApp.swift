526fdf1658a1d3306fe3dad1cdcddb39089a3769
Austin Burke <austinlayneburke@gmail.com>
2021-11-23 06:42:10 -0600

    Initial project commit

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Austin Burke on 11/23/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
