e56dd07f9c33b220cf83da83c5e41b35077b1933
Austin Burke <austinlayneburke@gmail.com>
2021-11-27 14:00:00 +0100

    Added json decoder methods and cover image model

---
+++ Africa/Modal/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Austin Burke on 11/29/21.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
