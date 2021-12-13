bc1544f990aa984cd551fdb040c472113e3d06e4
Austin Burke <austinlayneburke@gmail.com>
2021-12-13 05:42:05 -0600

    Complete heading view and add component to detail view

--- Africa/View/AnimalDetailView.swift
+++ Africa/View/AnimalDetailView.swift
@@ -18,12 +18,35 @@ struct AnimalDetailView: View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack(alignment: .center, spacing: 20) {
                 // HERO IMAGE
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
                 
                 // TITLE
+                Text(animal.name.uppercased())
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.primary)
+                    .background(
+                        Color.accentColor
+                            .frame(height: 6)
+                            .offset(y: 24)
+                    )
                 
                 // HEADLINE
+                Text(animal.headline)
+                    .font(.headline)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(.accentColor)
+                    .padding(.horizontal)
                 
                 // GALLERY
+                Group {
+                    HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Pictures")
+                }
+                .padding(.horizontal)
                 
                 // FACTS
                 
